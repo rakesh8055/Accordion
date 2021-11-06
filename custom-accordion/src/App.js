dd4324e2ad81adf66cb812dfeed290344a38b608
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-06 19:19:53 +0530

    Added temporary header and styles

--- custom-accordion/src/App.js
+++ custom-accordion/src/App.js
@@ -5,6 +5,9 @@ import AC_DATA from './data/data';
 function App() {
   return (
     <div className="App">
+      <div className='header'>
+        <h3>Public</h3>
+      </div>
       { AC_DATA.map((item) => (
         <Accordion title={item.title} text={item.text}/>
       ))
