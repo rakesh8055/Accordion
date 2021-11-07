b793522f60fe6b3fcd1a9cb9b731b08ec0432817
Rakesh K R <rakeshkumar8055@gmail.com>
2021-11-07 14:24:48 +0530

    Added firebase config for authentication

---
+++ custom-accordion/src/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyBWQZuaygV-PWHOclRlK3hqeDwM_XBjgHE",
+    authDomain: "custom-accordion.firebaseapp.com",
+    projectId: "custom-accordion",
+    storageBucket: "custom-accordion.appspot.com",
+    messagingSenderId: "253755513692",
+    appId: "1:253755513692:web:485c5330d15d46e8fe7f99",
+    measurementId: "G-Y8EW76W4P7"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+
+  export const signInWithGoogle = () => auth.signInwithPopup(provider);
+
+  export default firebase;
